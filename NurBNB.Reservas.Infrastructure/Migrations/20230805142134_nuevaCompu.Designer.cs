9e0e3479398f7091853d17c931fe3f35dff1f406
Iver Fernando Sandagorda Canedo <isandagord@bancosol.com.bo>
2023-08-05 10:48:44 -0400

    mejoras

--- NurBNB.Reservas.Infrastructure/Migrations/20230805142134_nuevaCompu.Designer.cs
+++ NurBNB.Reservas.Infrastructure/Migrations/20230805142134_nuevaCompu.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NurBNB.Reservas.Infrastructure.EF.Context;
 
@@ -10,9 +11,11 @@ using NurBNB.Reservas.Infrastructure.EF.Context;
 namespace NurBNB.Reservas.Infrastructure.Migrations
 {
     [DbContext(typeof(ReadDbContext))]
-    partial class ReadDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230805142134_nuevaCompu")]
+    partial class nuevaCompu
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.9");
