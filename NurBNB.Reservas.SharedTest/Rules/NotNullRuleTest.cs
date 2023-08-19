eff6ae9107d0f76227ffc9ea725f7b8589d42d0b
Iver Fernando Sandagorda Canedo <isandagord@bancosol.com.bo>
2023-08-18 20:20:22 -0400

    Add Test Rules

---
+++ NurBNB.Reservas.SharedTest/Rules/NotNullRuleTest.cs
@@ -0,0 +1,24 @@
+﻿using NurBNB.Reservas.SharedKernel.Rules;
+using NurBNB.Reservas.SharedKernel.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NurBNB.Reservas.SharedTest.Rules
+{
+    public class NotNullRuleTest
+    {
+        [Theory]
+        [InlineData(null, false)]
+        [InlineData("Fernando", true)]
+        public void ObjectoNulo(string valorEsperado, bool resultadoEsperado)
+        {
+            NotNullRule rule = new NotNullRule(valorEsperado);
+            bool esValido = rule.IsValid();
+
+            Assert.Equal(resultadoEsperado, esValido);
+        }
+    }
+}
