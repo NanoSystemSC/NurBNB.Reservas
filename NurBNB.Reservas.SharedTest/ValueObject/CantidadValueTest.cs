7a09bd0aea675fb647caccb3df7d213b2b309d17
Iver Fernando Sandagorda Canedo <isandagord@bancosol.com.bo>
2023-08-18 20:02:16 -0400

    Add Test

---
+++ NurBNB.Reservas.SharedTest/ValueObject/CantidadValueTest.cs
@@ -0,0 +1,46 @@
+using NurBNB.Reservas.SharedKernel.Core;
+using NurBNB.Reservas.SharedKernel.ValueObjects;
+
+namespace NurBNB.Reservas.SharedTest.ValueObject
+{
+    public class CantidadValueTest
+    {
+        [Theory]
+        [InlineData(0)]
+        [InlineData(20)]
+        public void ValorMayorIgualACero(int valorEsperado)
+        {
+            //int valorEsperado = 10;
+            CantidadValue valorAverificar = new CantidadValue(valorEsperado);
+
+            Assert.Equal(valorEsperado, valorAverificar.Value);
+        }
+
+        [Fact]
+        public void ValorMenorACero()
+        {
+            int valorEsperado = -5;
+            //CantidadValue valorAverificar = new CantidadValue(valorEsperado);
+
+            Assert.Throws<BussinessRuleValidationException>(() =>  new CantidadValue(valorEsperado));
+        }
+
+        [Fact]
+        public void DeEnteroACantidad()
+        {
+            int valorEsperado = 5;
+            CantidadValue valorAverificar =  (CantidadValue)valorEsperado;
+
+            Assert.Equal(valorEsperado, valorAverificar.Value);
+        }
+
+        [Fact]
+        public void DeCantidadAEntero()
+        {
+            CantidadValue valorAverificar = new CantidadValue(5);
+            int valorConvertido = valorAverificar;
+
+            Assert.Equal(valorAverificar.Value, valorConvertido);
+        }
+    }
+}
