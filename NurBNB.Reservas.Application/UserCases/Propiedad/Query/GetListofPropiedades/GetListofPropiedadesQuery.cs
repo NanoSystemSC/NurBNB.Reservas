03c831ab99035335f8116bc6313197252e8af527
ferfsc <ferfsc@gmail.com>
2024-02-05 23:17:39 -0400

    Add Listas con Join

---
+++ NurBNB.Reservas.Application/UserCases/Propiedad/Query/GetListofPropiedades/GetListofPropiedadesQuery.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using NurBNB.Reservas.Application.Dto.Propiedad;
+using NurBNB.Reservas.Domain.Model.Estados;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NurBNB.Reservas.Application.UserCases.Propiedad.Query.GetListofPropiedades
+{
+    public class GetListofPropiedadesQuery : IRequest<List<ListofPropiedadesDto>>
+    {
+	   public TipoEstadoReserva estadoReserva { get; set; }
+    }
+}
