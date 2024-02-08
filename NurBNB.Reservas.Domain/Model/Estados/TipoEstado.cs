98c30f818bae0b58837ded293fb891cec93231fb
Fernando Sandagorda Canedo <ferfsc@gmail.com>
2024-02-08 10:30:21 -0400

    eliminar comentarios

--- NurBNB.Reservas.Domain/Model/Estados/TipoEstado.cs
+++ NurBNB.Reservas.Domain/Model/Estados/TipoEstado.cs
@@ -8,10 +8,6 @@ namespace NurBNB.Reservas.Domain.Model.Estados
 {
     public enum TipoEstadoReserva
     {
-	   //Disponible = 1,
-	   //Solicitado = 2,
-	   //Reservado = 3,
-	   //Cancelado = 4
 	   Disponible,
 	   Solicitado,
 	   Reservado ,
